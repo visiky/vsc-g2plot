98f7b36f885640bb7c23a94846441705c7c43f1f
visiky <736929286@qq.com>
2021-06-14 20:38:27 +0800

    draft: 暂存右键添加资产

--- src/assetsProvider.ts
+++ src/assetsProvider.ts
@@ -25,7 +25,7 @@ export class PagesTreeDataProvider
     this.context = context;
   }
 
-  getIconPath(type: string) {
+  private getIconPath(type: string) {
     return {
       light: this.context.asAbsolutePath(
         path.join("resources", "light", `${type.toLowerCase()}.svg`)
@@ -35,8 +35,6 @@ export class PagesTreeDataProvider
       ),
     };
   }
-
-  // ,
 
   async getChildren(element: { key: string }) {
     return getChildren(element?.key).map((key) => getNode(key));
@@ -82,13 +80,123 @@ function getTreeItem(
   return {};
 }
 
+type PlotInfo = typeof PLOTS[0];
+ 
 export class AssetsProvider {
+  context: vscode.ExtensionContext;
+
   constructor(context: vscode.ExtensionContext) {
+    this.context = context;
     const pagesTreeDataProvider = new PagesTreeDataProvider(context);
     context.subscriptions.push(
       vscode.window.createTreeView("g2plotAssetView", {
         treeDataProvider: pagesTreeDataProvider,
-      })
+      }),
+      vscode.commands.registerCommand(
+        "g2plot.component.copy",
+        async (uri: PlotInfo) => {
+          if (typeof uri?.type === "string") {
+            vscode.window.showInformationMessage(`Copy ${uri.type}`);
+            await vscode.env.clipboard.writeText(uri.schema);
+          }
+        }
+      ),
+      vscode.commands.registerCommand('g2plot.assets.add', async (uri?: vscode.Uri) => {
+        await this.addAsset(null);
+      }),
     );
+  }
+  
+  /**
+   * 资产添加到 js/jsx/tsx 方法
+   *
+   * @param range 指定插入的位置
+   */
+   public async addAsset(asset: any, range?: vscode.Range) {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
+    // 开始添加...
+    const result = await vscode.window.withProgress(
+      {
+        location: vscode.ProgressLocation.Notification,
+        cancellable: false,
+        title: '添加资产',
+      },
+      async progress => {
+        console.log('[addAsset] addAsset Start');
+        progress.report({
+          message: '物料准备中',
+          increment: 10,
+        });
+        const assetContent = asset || (await vscode.env.clipboard.readText());
+
+        //     const { type: preparedAssetType, content: preparedAsset } =
+    //       getAssetType(assetContent) || {};
+    //     log('[addAsset] preparedAssetType', preparedAssetType);
+    //     if (!Object.keys(ResourceType).includes(preparedAssetType)) {
+    //       if (!this.panel?.visible) {
+    //         // 资产市场已打开
+    //         const selection = await vscode.window.showInformationMessage(
+    //           '未检测待添加的资产，是否打开资产市场查看？',
+    //           ...['否', '是'],
+    //         );
+    //         if (selection === '是') {
+    //           await vscode.commands.executeCommand('bigfish.assets.openAssets');
+    //         }
+    //       } else {
+    //         await vscode.window.showInformationMessage('未检测待添加的资产');
+    //       }
+    //       return;
+    //     }
+
+        if (!editor) {
+          return;
+        }
+    //     const addAssetFunc = {
+    //       [ResourceType.dumi]: this.addDumiAsset,
+    //       // React Component 资产使用 inline 方式添加
+    //       [ResourceType.reactComponent]: this.addDumiAsset,
+    //     }[preparedAssetType]?.bind(this);
+
+    //     if (!addAssetFunc) {
+    //       // 暂不支持的资产方法
+    //       await vscode.window.showErrorMessage(`暂不支持 ${preparedAssetType} 资产添加`);
+    //       return;
+    //     }
+
+    //     const content = editor.document.getText();
+    //     const ast = parse(content);
+
+        const insertRange = range || new vscode.Range(editor.selection.start, editor.selection.end);
+
+        console.log('[addAsset] start');
+        progress.report({
+          message: '正在添加',
+          increment: 20,
+        });
+        console.log('[addAssets] progress 10');
+        try {
+          // todo 添加
+          // await addAssetFunc(preparedAsset, insertRange, {
+          //   progress,
+          //   editor,
+          //   ast,
+          // });
+        } catch (e) {
+          console.error('[ERROR] addAssetFunc', e);
+          console.log('addAssetFunc', e.stack);
+          await vscode.window.showErrorMessage('添加失败');
+        }
+
+        return true;
+      },
+    );
+
+    if (result && editor.document.isDirty && !(await editor.document.save())) {
+      // 保存一下
+      await vscode.window.showInformationMessage('资产添加成功');
+    }
   }
 }
